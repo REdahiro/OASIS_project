637cdd3240a6461aee4d31dde1876abe100dd640
REdahiro <63489392+REdahiro@users.noreply.github.com>
2025-06-29 14:56:06 +0900

    Update tensorQTL_cis_nominal.py

--- scripts/cis-eQTL_mapping/tensorQTL_cis_nominal.py
+++ scripts/cis-eQTL_mapping/tensorQTL_cis_nominal.py
@@ -49,7 +49,7 @@ path = f'{base_path}{l1_cluster}/{cluster_resolution}/'
 
 # chr list: only autosomal
 chr_list = ['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10',
-	        'chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20',
+	    'chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20',
             'chr21','chr22']
 
 KF_ID = {'SG_XXXXX'}      # sample ID removed from chrX analysis
